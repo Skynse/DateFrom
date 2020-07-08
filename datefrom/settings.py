bb99a1a89c621b8e5cf09fafa4119f700c3d716e
Skynse <austinamakyeansah@gmail.com>
2020-07-08 00:17:13 +0000

    update settings.py

--- datefrom/settings.py
+++ datefrom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%vituc(q&gqa&bik@2e()x2a+dmj%sy(6krkxn$!97snoh-tr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['datefrom.herokuapp.com']
 
